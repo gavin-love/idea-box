f050fa40c48ef8e35e77a8541eac30de886a7fe1
co.loco <1co.loco@oprotonmail.ch>
2018-02-12 17:34:49 -0700

    Create constructor function

--- scripts.js
+++ scripts.js
@@ -1,40 +1,30 @@
 
+var cardTitle = $(".title-input")
+var cardBody = $(".body-input")
 
+function Card(title, body, id, quality) {
+  this.title = title
+  this.body = body
+  this.id = id || $.now()
+  this.quality = quality || "swill"
+}
 
+function cardPrepend(newCard) {
+  $(".ideas-article").prepend(`<article id="${newCard.id}" class="idea-card">
+        <h2 class="card-title">${newCard.title}</h2>
+        <button class="delete-button">Delete</button>
+        <p class="idea-card-body">${newCard.body}</p>
+        <button class="upvote-button">upvote</button>
+        <button class="downvote-button">downvote</button>
+        <p class="quality">quality:</p>
+        <p class="quality-value">${newCard.quality}</p>
+        <hr>
+      </article>`)
+}
+$('button').on('click', saveButton)
+function saveButton(e) {
+  e.preventDefault()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$('button').on('click', function() {
-  console.log("hello");
-});
+  var newCard = new Card(cardTitle.val(), cardBody.val())
+  cardPrepend(newCard)
+}
